2c65f081d35442cc8d30e011452036819c0c1a90
daniel <danielnaveda88@gmail.com>
2017-03-06 12:23:32 +0100

    Improving Users seeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,8 +11,36 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->insert([
-            'name' => 'user_'.str_random(10),
-        ]);
+        // DB::table('users')->insert(['name' => 'user_'.str_random(10)]);
+        DB::table('users')->insert(['name' => 'Maile Nolting']);
+        DB::table('users')->insert(['name' => 'Mckenzie Gano']);
+        DB::table('users')->insert(['name' => 'Lou Oswalt']);
+        DB::table('users')->insert(['name' => 'Adrienne Sikes']);
+        DB::table('users')->insert(['name' => 'Arie Corley']);
+        DB::table('users')->insert(['name' => 'Tressie Inks']);
+        DB::table('users')->insert(['name' => 'Nickole Bukowski']);
+        DB::table('users')->insert(['name' => 'Lacey Cumbee']);
+        DB::table('users')->insert(['name' => 'Faith Troxler']);
+        DB::table('users')->insert(['name' => 'Chere Campas']);
+        DB::table('users')->insert(['name' => 'Lu Lesage']);
+        DB::table('users')->insert(['name' => 'Courtney Koester']);
+        DB::table('users')->insert(['name' => 'Lanell Broussard']);
+        DB::table('users')->insert(['name' => 'Silvia Ballweg']);
+        DB::table('users')->insert(['name' => 'Barrett Wesson']);
+        DB::table('users')->insert(['name' => 'Margery Geiger']);
+        DB::table('users')->insert(['name' => 'Vita Gerken']);
+        DB::table('users')->insert(['name' => 'Abram Lazar']);
+        DB::table('users')->insert(['name' => 'Dorothy Bisbee']);
+        DB::table('users')->insert(['name' => 'Rusty Squier']);
+        DB::table('users')->insert(['name' => 'Inocencia Mckee']);
+        DB::table('users')->insert(['name' => 'Cinthia Manno']);
+        DB::table('users')->insert(['name' => 'Felicidad Hauptman']);
+        DB::table('users')->insert(['name' => 'Margarett Parshall']);
+        DB::table('users')->insert(['name' => 'Nona Dyke']);
+        DB::table('users')->insert(['name' => 'Jeffery Ecklund']);
+        DB::table('users')->insert(['name' => 'Berta Batson']);
+        DB::table('users')->insert(['name' => 'Robbi Selle']);
+        DB::table('users')->insert(['name' => 'Tyesha Newhard']);
+        DB::table('users')->insert(['name' => 'Angelo Ellsworth']);
     }
 }
