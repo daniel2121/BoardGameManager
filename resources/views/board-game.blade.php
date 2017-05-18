55ff8c0da3967bfbbbdbc61262064589efbbc548
daniel <danielnaveda88@gmail.com>
2017-05-18 22:49:43 +0200

    Viewing the cover

--- resources/views/board-game.blade.php
+++ resources/views/board-game.blade.php
@@ -32,7 +32,7 @@ tr:nth-child(even) {
     <td><a href="{{URL::to('/')}}/board-game/{{$board_game->id}}">{{$board_game->id}}</a></td>
     <td><a href="{{URL::to('/')}}/board-game/{{$board_game->id}}">{{$board_game->name}}</a></td>
     <td><a href="{{URL::to('/')}}/board-game/{{$board_game->id}}">{{$board_game->designers}}</a></td>
-    <td><a href="{{URL::to('/')}}/board-game/{{$board_game->id}}">{{$board_game->cover}}</a></td>
+    <td><a href="{{URL::to('/')}}/images/{{$board_game->cover}}">{{$board_game->cover}}</a></td>
   </tr>
 </table>
 
